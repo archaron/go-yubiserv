39a21b77f9b84b0458f1db3665c69cda254bda3c
Alexander Tischenko <tsm@fiberside.ru>
2020-09-28 07:36:36 +0300

    Scripts fix

--- build/build.sh
+++ build/build.sh
@@ -13,6 +13,9 @@ cp ./contrib/system.d/yubiserv.service $RELEASEDIR/build/$UNITDIR
 cp ./contrib/debian-repo-config $RELEASEDIR/deb/conf/distributions
 cp ./contrib/yumrepo.repo $RELEASEDIR/rpm/yubiserv.repo
 
+touch $RELEASEDIR/build/opt/yubiserv/etc/yubiserv.yaml
+
+
 
 # Build packages
-fpm -s dir -t deb -n yubiserv --deb-user="nobody" --deb-group="nogroup" --license=MIT --architecture="amd64" --vendor="Alexander Tischenko <tsm@fiberside.ru>" --deb-systemd=$RELEASEDIR/build/$UNITDIR/yubiserv.service --config-files=/opt/yubiserv/etc/yubiserv.yaml --version $VERSION --iteration $BUILD --depends sqlite3 --description "YubiServ - Cloud-free YubiKey verification service." --before-install $BUILD_DIR/contrib/beforeinstall.sh --after-install $BUILD_DIR/contrib/afterinstall.sh --before-remove $BUILD_DIR/contrib/beforeremove.sh --after-upgrade $BUILD_DIR/contrib/afterupgrade.sh -p $RELEASEDIR/deb -C $RELEASEDIR/build .
+fpm -s dir -t deb -n yubiserv --deb-user="nobody" --deb-group="nogroup" --license=MIT --architecture="amd64" --vendor="Alexander Tischenko <tsm@fiberside.ru>" --deb-systemd=$RELEASEDIR/build/$UNITDIR/yubiserv.service --config-files=/opt/yubiserv/etc --version $VERSION --iteration $BUILD --depends libsqlite3-0 --description "YubiServ - Cloud-free YubiKey verification service." --before-install $BUILD_DIR/contrib/beforeinstall.sh --after-install $BUILD_DIR/contrib/afterinstall.sh --before-remove $BUILD_DIR/contrib/beforeremove.sh --after-upgrade $BUILD_DIR/contrib/afterupgrade.sh -p $RELEASEDIR/deb -C $RELEASEDIR/build .
--- contrib/afterinstall.sh
+++ contrib/afterinstall.sh
@@ -2,6 +2,6 @@
 export USER="nobody"
 export GROUP="nogroup"
 id -u $USER &>/dev/null || useradd $USER
-id -g $GROUP &>/dev/null || groupadd $GROUP
-
+id -g $USER &>/dev/null || groupadd $GROUP
+chown $USER:$GROUP /opt/yubiserv/etc/yubiserv.yaml
 systemctl daemon-reload
